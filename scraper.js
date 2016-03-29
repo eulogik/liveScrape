debda17793ed6afa81d9b0302ba79abb647bb0a5
Gautam Kishore <gautam@eulogik.com>
2016-03-29 12:09:24 +0530

    built basic amazon scraper
    using cheerio & artoo-js on node

---
+++ scraper.js
@@ -0,0 +1,24 @@
+var request = require('request');
+var cheerio = require('cheerio');
+var artoo = require('artoo-js');
+
+var url = "http://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=b&lo=apparel&rh=i%3Aaps%2Ck%3Ab";
+var params = {url: 'href', title: 'text', img: function(){return $(this).parent().parent().parent().find('img.s-access-image').attr('src')}};
+var repeatedNode = '#s-results-list-atf a.s-access-detail-page';
+
+request(url, function(err, resp, body) {
+        if (err)
+            throw err;
+        $ = cheerio.load(body);
+        // Setting artoo's context
+        artoo.setContext($);
+
+        //scraping goes here!
+        var data = artoo.scrape(repeatedNode, params);
+        console.log(data);
+        // $('#s-results-list-atf a.s-access-detail-page').each(function() {
+        //     console.log($(this).text());
+        // });
+
+
+    });

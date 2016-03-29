6a6c1408764aebce45c93344d134c956d52772f3
Gautam Kishore <gautam@eulogik.com>
2016-03-29 17:19:16 +0530

    url params defined

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ app.get('/scrape', function(req, res){
   var repeatedNode = req.param('repeated-node');;
   var params = req.param('params');
 
-/*
-url = "http://www.amazon.in/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=b&lo=apparel&rh=i%3Aaps%2Ck%3Ab";
+
+url = "http://www.amazon.in/s/ref=sr_pg_6?rh=i%3Aaps%2Ck%3Acomputer+table&page=6&keywords=computer+table&ie=UTF8&qid=1459248975&spIA=B015W1X4A4,B015W1X45E,B01683PX36,B01683PK8Y,B019A5PKWS,B01683PPTS,B01D1N852W,B0198NC5DO&lo=apparel";
 repeatedNode = '#s-results-list-atf a.s-access-detail-page';
 params = {url: 'href', title: 'text', img: function(){return $(this).parent().parent().parent().find('img.s-access-image').attr('src')}, category: function(){return $(this).parent().parent().find('a.a-size-small>span.a-text-bold').text().slice(0, -1);}};
-*/
+/**/
 request(url, function(err, resp, body) {
         if (err)
             throw err;
@@ -31,5 +31,5 @@ request(url, function(err, resp, body) {
     });
 });
 app.listen('8081')
-console.log('Magic happens on port 8081');
+console.log('It\'s cooking on port 8081');
 exports = module.exports = app;
